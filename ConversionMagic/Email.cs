067c7eacfc301e4af06cd735c3af6a472be49e7e
Abhishek <Abhishek@DESKTOP-RONAPNL>
2023-02-21 18:08:34 +0530

    Add project files.

---
+++ ConversionMagic/Email.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionMagic
+{
+    internal class Email
+    {
+        public string Value { get; init; }
+
+        public Email(string value) => Value = value;
+
+        public static implicit operator string(Email email) => email.Value;
+
+        public static explicit operator Email(string value) => new(value);
+    }
+}
---
+++ ConversionMagic/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConversionMagic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Email email = new("programmingduniya1@gmail.com");
+
+        string strEmail = email;
+
+        Email newEmail = (Email) "programmingduniya1@gmail.com";
+    }
+}
